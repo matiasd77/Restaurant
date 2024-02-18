48bd241710692403ffede12975760db2fb7c7ed7
matiasd77 <durollarimatias@gmail.com>
2024-02-18 11:51:31 +0100

    Fillimi projektit

---
+++ Restaurant/src/main/java/Restaurant/Project/Restaurant/RestaurantApplication.java
@@ -0,0 +1,13 @@
+package Restaurant.Project.Restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
